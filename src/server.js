a4dbb7765c7ba2cc280c2f3780674a7e5de7accb
eduqg <eduardoqgomes@gmail.com>
2019-10-01 12:33:20 -0300

    Connecting with MongoDB

--- src/server.js
+++ src/server.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
 // Colocar ./ no require pois se não irá procurar nos node_modules
 const routes = require('./routes');
 
 const app = express();
+mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-aawtm.mongodb.net/test?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 app.use(express.json());
 app.use(routes);
 
