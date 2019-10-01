6c5bad1585b14003c96c9eaaeadcbf62b7db690c
eduqg <eduardoqgomes@gmail.com>
2019-10-01 14:38:32 -0300

    Creating Spots Model and Controller

--- src/controllers/SessionController.js
+++ src/controllers/SessionController.js
@@ -5,7 +5,13 @@ const User = require('../models/User');
 module.exports = {
   async store(req, res) {
     const { email } = req.body;
-    const user = await User.create({ email });
+
+    let user = await User.findOne({ email });
+
+    if (!user) {
+      user = await User.create({ email });
+    }
+    
     return res.json(user);
   }
 };
