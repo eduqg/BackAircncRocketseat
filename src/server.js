327d3c67066d43cdd4c8f1c8e0d9d7a77db335db
eduqg <eduardoqgomes@gmail.com>
2019-10-02 17:28:46 -0300

    Adding cors only to react front

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 // Colocar ./ no require pois se não irá procurar nos node_modules
 const routes = require('./routes');
 
@@ -8,6 +9,7 @@ mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-aawtm.mongodb.net/t
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+app.use(cors({ origin: 'http://localhost:3000' }));
 app.use(express.json());
 app.use(routes);
 
